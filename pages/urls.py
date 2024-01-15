de7f0e8880cb2070bb08a0a6e3e76041dc717f7b
Sakshi <sakshisingh00200@gmail.com>
2024-01-16 05:08:18 +0530

    'initial-commit'

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import say_hello
+from .views import say_hello, HomePageView, AboutPageView
 
 #urlpattern
 urlpatterns = [
-    path('', say_hello)
+    path('hello/', say_hello),
+    path('about/', AboutPageView.as_view(), name = 'about'),
+    path('', HomePageView.as_view(), name='home')
 ]
--- project/settings.py
+++ project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'], #new
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
